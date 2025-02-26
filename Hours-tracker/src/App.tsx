3ba5534e3b3ea4b2d72fb19362d97802270752d5
kutho53 <kurtisthomas53@gmail.com>
2025-02-26 18:22:04 -0500

    react setup

---
+++ Hours-tracker/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hours Tracker</h1>
+      <div className="card">
+        <button>
+          
+        </button>
+        <p>
+          
+        </p>
+      </div>
+      <p>
+        
+      </p>
+    </>
+  )
+}
+
+export default App
