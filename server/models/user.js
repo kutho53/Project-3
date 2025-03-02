5a09a060fb484fc6adc3b6b14df8480d616a9220
kutho53 <kurtisthomas53@gmail.com>
2025-03-02 18:06:59 -0500

    merge main

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,13 @@
-import
+const { Schema, model } = require('mongoose');
+
+//add the types and reqirements for User
+// defer to data in database
+
+//import and add Client schema 
+const userSchema = new Schema(
+    {}
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
